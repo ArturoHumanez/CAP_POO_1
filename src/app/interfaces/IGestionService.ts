b8dc8b518ee3e6f4826cf365cc2dab586e4966e1
arturo.humanez <arturo.humanez@axity.com>
2025-02-11 11:39:38 -0600

    Feat: Explicación Interfaz

--- src/app/interfaces/IGestionService.ts
+++ src/app/interfaces/IGestionService.ts
@@ -1,3 +1,6 @@
+/**
+ * Interfaz encargada de establecer el ocntrato de funciones que deberán tener los servicios de gestión
+ */
 export interface IGestionService<T>{
     agregar(elmento: T): void,
     consultar(): void,
